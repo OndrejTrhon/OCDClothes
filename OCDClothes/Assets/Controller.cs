0c3f851ce62778ce379f6678a47e68645f0cea1c
Ondřej Trhoň <ondrej@trhon.cz>
2019-01-25 23:57:36 +0100

    Updates
    - first version of random repeater (courutine)
    - stuck at destroying cloned objects blah

--- OCDClothes/Assets/Controller.cs
+++ OCDClothes/Assets/Controller.cs
@@ -8,10 +8,12 @@ public class Controller : MonoBehaviour
     public GameObject SoundPlayer;
     public GameObject text1;
     public int time;
+    private int repeat = 0;
+    public int destroyTime;
     // Start is called before the first frame update
     void Start()
     {
-        Invoke("CreateSoundPlayer", time);
+        StartCoroutine(RepeatSound());
     }
 
     // Update is called once per frame
@@ -21,11 +23,25 @@ public class Controller : MonoBehaviour
             Instantiate(text1, Vector3.zero, Quaternion.identity);
              Debug.Log("ok");
         }
+       repeat = Random.Range(0, 5);
+       Debug.Log(repeat);
     }
 
-    void CreateSoundPlayer()
-     {
-     Instantiate(SoundPlayer, Vector3.zero, Quaternion.identity);
+    IEnumerator RepeatSound() {
+     while (true) {
+        CreateSoundPlayer();
+        yield return new WaitForSeconds(repeat);
+
+    }
+    }
+
+    void CreateSoundPlayer() {
+        Instantiate(SoundPlayer, Vector3.zero, Quaternion.identity);
+        DestroySoundPlayer();
+    }
+
+    void DestroySoundPlayer() {
+        Destroy(SoundPlayer,destroyTime);
     }
 
 }
