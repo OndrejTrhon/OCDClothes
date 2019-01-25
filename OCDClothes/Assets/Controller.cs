48d8122028c49e3643c14d1adfe7a8025f721488
Ondřej Trhoň <ondrej@trhon.cz>
2019-01-25 23:24:09 +0100

    First push!
    Little snapshot of the project
    - bg pic
    - camera set
    - first try on controller script
    - just working snapshot

---
+++ OCDClothes/Assets/Controller.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+
+{
+    public GameObject SoundPlayer;
+    public GameObject text1;
+    public int time;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("CreateSoundPlayer", time);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && GameObject.FindWithTag("SoundOn") != null) {
+            Instantiate(text1, Vector3.zero, Quaternion.identity);
+             Debug.Log("ok");
+        }
+    }
+
+    void CreateSoundPlayer()
+     {
+     Instantiate(SoundPlayer, Vector3.zero, Quaternion.identity);
+    }
+
+}
