5c72f4bc37c4d93e98f4d4afbff687530ffdc8ba
Ondřej Trhoň <ondrej@trhon.cz>
2019-01-27 20:17:02 +0100

    Logic reworks / added code for intro/outro / counting levels
    Anxiety count
    ClothingLevel up to 4 (need fix)

--- OCDClothes/Assets/Controller.cs
+++ OCDClothes/Assets/Controller.cs
@@ -5,10 +5,22 @@ using UnityEngine;
 public class Controller : MonoBehaviour
 
 {
+    public static bool InputEnabled = false;
     public GameObject SoundPlayer;
     public GameObject text1;
     public int time;
     public Animator animShirt;
+
+    public GameObject Intro1;
+    public GameObject Intro2;
+    public GameObject Intro3;
+    public GameObject Outro1;
+    public GameObject Outro2;
+    public GameObject Outro_0pct;
+    public GameObject Outro_25pct;
+    public GameObject Outro_50pct;
+    public GameObject Outro_75pct;
+    public GameObject Outro_100pct;
 
     private int repeat = 0;
     private int anxietyLevel = 0;
@@ -16,7 +28,10 @@ public class Controller : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(RepeatSound());
+        Invoke("ShowIntro1",0);
+        Invoke("ShowIntro2",6);
+        Invoke("ShowIntro3",9);
+        Invoke("ShowGame",12);
         
 
     }
@@ -26,27 +41,29 @@ public class Controller : MonoBehaviour
     {
         //Main function that tracks anxiety and future level count??
        
+        if(InputEnabled) {
 
-        //For future level counting?
-         if (Input.GetKeyDown(KeyCode.E) && GameObject.FindWithTag("SoundOn") == null) {
-            clothingLevel = clothingLevel +1;
-             Debug.Log(clothingLevel);
-        }
-
-        if (Input.GetKeyDown(KeyCode.E))
+        if (Input.GetKeyDown(KeyCode.E) && clothingLevel < 4)
         {
             if (null != animShirt)
             {
                 // Play animation t-shirt state (set to idle upon entry in animation structure)
                 animShirt.Play("animation_tshirt");
                 StartCoroutine(PuttingOn());
+                clothingLevel = clothingLevel +1;
+            } else {
+                Invoke("ShowOutro1",0);
             }
+            
         }
 
        repeat = Random.Range(0, 10);
        //Debug.Log(repeat);
-    }
 
+    }
+               // Debug.Log(InputEnabled);
+
+    }
     IEnumerator RepeatSound() {
      while (true) {
         CreateSoundPlayer();
@@ -69,9 +86,70 @@ public class Controller : MonoBehaviour
                     Instantiate(text1);
                     anxietyLevel = anxietyLevel +1;
                     clothingLevel = clothingLevel +1;
-                Debug.Log(anxietyLevel);
+                                   Debug.Log("AnxietyLevel " + anxietyLevel);
+                                   
+                                   Debug.Log("ClothingLevel  " + clothingLevel);
+
+                //Debug.Log(anxietyLevel);
                 }
             yield return null;
         }
+    }
+
+    void ShowIntro1() {
+                    Intro1.SetActive(true);
+    }
+    
+    void ShowIntro2() {
+                    Intro1.SetActive(false);
+                    Intro2.SetActive(true);
+    }
+    
+    void ShowIntro3() {
+                    Intro2.SetActive(false);
+                    Intro3.SetActive(true);
+    }
+
+    void ShowGame() {
+                    Intro3.SetActive(false);
+                    StartCoroutine(RepeatSound());
+                    InputEnabled = true;
+
+
+    }
+    	
+    void ShowOutro1() {
+                    InputEnabled = false;
+                    Outro1.SetActive(true);
+                    StopCoroutine(RepeatSound());
+                        if (anxietyLevel == 0) {
+                            Outro_0pct.SetActive(true);
+                        }
+
+                        if (anxietyLevel == 1) {
+                            Outro_25pct.SetActive(true);
+                        }
+
+                        if (anxietyLevel == 2) {
+                            Outro_50pct.SetActive(true);
+                        }
+
+                        if (anxietyLevel == 3) {
+                            Outro_75pct.SetActive(true);
+                        }
+
+                        if (anxietyLevel == 4) {
+                            Outro_100pct.SetActive(true);
+                        }
+
+                    Invoke("ShowOutro2", 6);
+
+    }
+    void ShowOutro2() {
+                    Intro2.SetActive(false);
+                    Intro3.SetActive(true);
+                    
+
+
     }
 }
