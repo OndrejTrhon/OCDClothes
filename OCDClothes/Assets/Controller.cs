575ce44b8b5ab69c14fb086f6ab0620c3660b040
Ondřej Trhoň <ondrej@trhon.cz>
2019-01-29 12:10:36 +0100

    Sound pattern logic (contributed by @petrroll)
    new array function for playing sound in patterns and patterned delay
    - future idea: use array indices to play different sounds at different pattern positions

--- OCDClothes/Assets/Controller.cs
+++ OCDClothes/Assets/Controller.cs
@@ -27,15 +27,19 @@ public class Controller : MonoBehaviour
     private int anxietyLevel = 0;
     private int clothingLevel = 0;
     private static bool GameStateControl = false;
+
+    private int[] delays = new int[] {3, 4, 5, 6};
+    private int indexInDelays = 0;
+
     // Start is called before the first frame update
     void Start()
     {
         Invoke("ShowIntro1",0);
-        Invoke("ShowIntro2",4);
-        Invoke("ShowIntro3",8);
-        Invoke("ShowGame",12);
-        
+        Invoke("ShowIntro2",6);
+        Invoke("ShowIntro3",11);
+        Invoke("ShowGame",16);
 
+        repeat = delays[indexInDelays];
     }
 
     // Update is called once per frame
@@ -47,11 +51,14 @@ public class Controller : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.E)) {
 
-            if (clothingLevel < 4) {
+            if (clothingLevel < 3) {
                 // Play animation t-shirt state (set to idle upon entry in animation structure)
+                clothingLevel = clothingLevel +1;
                 animShirt.Play("animation_tshirt");
                 StartCoroutine(PuttingOn());
-                clothingLevel = clothingLevel +1;
+               Debug.Log("AnxietyLevel " + anxietyLevel);
+                                   
+                                   Debug.Log("ClothingLevel  " + clothingLevel);
             } else {
                 Invoke("ShowOutro1",1);
 
@@ -59,18 +66,21 @@ public class Controller : MonoBehaviour
             
             } }
 
-       repeat = Random.Range(0, 10);
+       //repeat = Random.Range(0, 10);
        //Debug.Log(repeat);
 
     }
                // Debug.Log(InputEnabled);
 
     IEnumerator RepeatSound() {
-     while (true) {
-        CreateSoundPlayer();
-        yield return new WaitForSeconds(repeat);
-
-    }
+        while (true) {
+            CreateSoundPlayer();
+            
+            indexInDelays += 1; 
+            indexInDelays = indexInDelays % delays.Length; 
+            reapat = delays[indexInDelays]; 
+            yield return new WaitForSeconds(repeat);
+        }
     }
 
     void CreateSoundPlayer() {
@@ -83,73 +93,67 @@ public class Controller : MonoBehaviour
             while(timer < time && GameObject.FindWithTag("AnxietyUpUp") == null) {
                 timer += Time.deltaTime;
                  
-                 if (GameObject.FindWithTag("SoundOn") != null) {
+                if (GameObject.FindWithTag("SoundOn") != null) {
                     Instantiate(text1);
                     anxietyLevel = anxietyLevel +1; 
-                                   Debug.Log("AnxietyLevel " + anxietyLevel);
                                    
-                                   Debug.Log("ClothingLevel  " + clothingLevel);
 
-                //Debug.Log(anxietyLevel);
+                    //Debug.Log(anxietyLevel);
                 }
             yield return null;
         }
     }
 
     void ShowIntro1() {
-                    Intro1.SetActive(true);
+        Intro1.SetActive(true);
     }
     
     void ShowIntro2() {
-                    Intro1.SetActive(false);
-                    Intro2.SetActive(true);
+        Intro1.SetActive(false);
+        Intro2.SetActive(true);
     }
     
     void ShowIntro3() {
-                    Intro2.SetActive(false);
-                    Intro3.SetActive(true);
+        Intro2.SetActive(false);
+        Intro3.SetActive(true);
     }
 
     void ShowGame() {
-                    Intro3.SetActive(false);
-                    StartCoroutine(RepeatSound());
-                    GameStateControl = true;
-                    InputEnabled = true;
-
-
+        Intro3.SetActive(false);
+        StartCoroutine(RepeatSound());
+        GameStateControl = true;
+        InputEnabled = true;
     }
     	
     void ShowOutro1() {
-                    InputEnabled = false;
-                    Outro1.SetActive(true);
-                    StopCoroutine(RepeatSound());
-                        if (anxietyLevel == 0) {
-                            Outro_0pct.SetActive(true);
-                        }
+    
+        StopCoroutine(RepeatSound());
+        
+        InputEnabled = false;
+        Outro1.SetActive(true);
 
-                        if (anxietyLevel == 1) {
-                            Outro_25pct.SetActive(true);
-                        }
+        if (anxietyLevel == 0) {
+            Outro_0pct.SetActive(true);
+        }
+        else if (anxietyLevel == 1) {
+            Outro_25pct.SetActive(true);
+        }
+        else if (anxietyLevel == 2) {
+            Outro_50pct.SetActive(true);
+        }
+        else if (anxietyLevel == 3) {
+            Outro_75pct.SetActive(true);
+        }
+        else if (anxietyLevel == 4) {
+            Outro_100pct.SetActive(true);
+        }
 
-                        if (anxietyLevel == 2) {
-                            Outro_50pct.SetActive(true);
-                        }
-
-                        if (anxietyLevel == 3) {
-                            Outro_75pct.SetActive(true);
-                        }
-
-                        if (anxietyLevel == 4) {
-                            Outro_100pct.SetActive(true);
-                        }
-
-                    Invoke("ShowOutro2", 6);
-
+        Invoke("ShowOutro2", 6);
     }
+
     void ShowOutro2() {
-                    Outro1.SetActive(false);
-                    Outro2.SetActive(true);
-                    Outro_pct_texts.SetActive(false);
-                    
+        Outro1.SetActive(false);
+        Outro2.SetActive(true);
+        Outro_pct_texts.SetActive(false);            
     }
 }
